8535ade17ae820491e8da0a74937a8ace8640372
Mads Lundsby Bisgaard <mads@malbi.dk>
2023-08-28 11:56:33 +0200

    Fix bigger cards not being bigger

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 export default function Card(props) {
   return (
     <article
-      className={`col-span-${props.isBig ? 2 : 1} row-span-${
-        props.isBig ? 2 : 1
+      className={`${
+        props.isBig ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
       }`}
     >
       <img
-        className="aspect-[3/4] object-cover"
+        className="aspect-[3/4] object-cover lg:aspect-[4/3]"
         src={`/assets/images/${props.img}`}
         alt=""
       />
