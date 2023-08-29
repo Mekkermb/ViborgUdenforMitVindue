33b414ea8703c7339fe5ceb5736a889f29125b4d
Mads Lundsby Bisgaard <mads@malbi.dk>
2023-08-29 11:19:58 +0200

    Add way to view videos when clicking on imgs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
 export default function Card(props) {
+  const handleImageClick = () => {
+    props.setEnlargedImage(`/assets/videos/${props.video}`);
+  };
+
   return (
     <article
       className={`${
         props.isBig ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
       }`}
+      onClick={handleImageClick}
     >
       <img
         className="aspect-[3/4] object-cover lg:aspect-[4/3]"
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import Card from "../components/Card";
 import Hero from "../components/Hero";
 import Back from "../components/Back";
@@ -5,14 +7,40 @@ import Footer from "../components/Footer";
 import data from "../data/data";
 
 export default function Gallery() {
+  const [enlargedImage, setEnlargedImage] = useState(null);
+
   const cards = data.map((card) => {
-    return <Card img={card.img} isBig={card.isBig} key={card.id} />;
+    return (
+      <Card
+        img={card.img}
+        video={card.video}
+        isBig={card.isBig}
+        key={card.id}
+        setEnlargedImage={setEnlargedImage}
+      />
+    );
   });
 
   return (
     <main className="relative bg-[#480CA8]">
       <Back />
       <Hero />
+      {enlargedImage && (
+        <section className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-75">
+          <video
+            className="max-h-full max-w-full"
+            src={enlargedImage}
+            alt="Enlarged"
+            autoPlay
+          />
+          <button
+            onClick={() => setEnlargedImage(null)}
+            className="absolute right-8 top-4 rounded bg-black bg-opacity-50 px-4 py-2 text-xl text-white lg:text-4xl"
+          >
+            Tilbage
+          </button>
+        </section>
+      )}
       <div className="flex flex-col">
         <section className="grid max-w-[66vw] grid-cols-4 grid-rows-2 self-end overflow-hidden rounded-bl-2xl shadow-2xl">
           {cards}
